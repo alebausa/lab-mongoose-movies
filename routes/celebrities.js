322d4b0242feef73885b796c1012dfc09e44182d
Ale Bausa <ale.bausa@gmail.com>
2019-08-21 09:08:25 +0200

    add add and delete operations

---
+++ routes/celebrities.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const Celebrity = require("../models/Celebrity");
+const mongoose = require("mongoose");
+
+/* GET celebrities main page. */
+router.get('/', (req, res, next) => {
+    Celebrity.find()
+        .then((celebrities) => {
+            console.log('celebrities', celebrities);
+            res.render('celebrities/index', { celebrities });
+        })
+        .catch(next);
+});
+
+
+/* GET create new celebrity*/
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new');
+});
+
+/* POST for the new celebrity*/
+router.post('/', (req, res, next) => {
+    const {
+        name, occupation, catchPhrase,
+    } = req.body;
+    Celebrity.create({
+        name,
+        occupation,
+        catchPhrase,
+    })
+        .then((celebrity) => {
+            console.log('celebrity ', celebrity);
+            res.redirect('/celebrities');
+        })
+        .catch(next);
+});
+
+/* GET celebrity detail */
+router.get('/:celebrityId', (req, res, next) => {
+    const { celebrityId } = req.params;
+
+    Celebrity.findById(celebrityId)
+        .then((celebrity) => {
+            res.render('celebrities/show', { celebrity });
+        })
+        .catch(next);
+});
+
+/*POST to edit celebrity*/
+router.get('/:celebrityId/edit', (req, res, next) => {
+    const { celebrityId } = req.params;
+    Celebrity.findById(celebrityId)
+        .then((celebrity) => {
+            res.render('celebrities/edit', celebrity);
+        })
+        .catch(next);
+});
+
+router.post('/:celebrityId', (req, res, next) => {
+    const { celebrityId } = req.params;
+    const {
+        name, occupation, catchPhrase, 
+    } = req.body;
+    Celebrity.findByIdAndUpdate(celebrityId)
+        .then((celebrity) => {
+            console.log('editing celebrity ', celebrity)
+            res.redirect(`/celebrities/${celebrityId}`);
+        })
+        .catch(next);
+});
+
+/*POST to delete celebrity*/
+router.post('/:celebrityId/delete', (req, res, next) => {
+    const { celebrityId } = req.params;
+    Celebrity.findByIdAndDelete(celebrityId)
+        .then(() => {
+            res.redirect('/celebrities');
+        })
+        .catch(next);
+});
+
+module.exports = router;
